3ed29ebce0c8ce9eb7c83e7db29e9f957bfe1425
Matt <rocketknight1@gmail.com>
2024-02-08 17:40:55 +0000

    Tests pass!

--- src/transformers/generation/__init__.py
+++ src/transformers/generation/__init__.py
@@ -222,8 +222,8 @@ if TYPE_CHECKING:
             MaxTimeCriteria,
             StoppingCriteria,
             StoppingCriteriaList,
-            validate_stopping_criteria,
             StopStringCriteria,
+            validate_stopping_criteria,
         )
         from .utils import (
             BeamSampleDecoderOnlyOutput,
